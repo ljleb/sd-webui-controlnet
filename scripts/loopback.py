5a74ec383cb08ba5e46a1f54e71b4fdfec7bdf90
ljleb <lebel.louisjacob@gmail.com>
2023-04-02 02:55:47 -0400

    basic work

--- scripts/loopback.py
+++ scripts/loopback.py
@@ -1,18 +1,76 @@
-from PIL import Image
-
-from modules import scripts, script_callbacks, processing, shared, devices
+from modules import scripts, processing, shared, devices
 import gradio as gr
 import torchvision
-import numpy
+
+from modules import img2img
 
 
-img2img_batch_loopback = gr.Checkbox(
-    label='Batch loopback',
-    value=False,
-    elem_id='controlnet_img2img_batch_loopback')
+img2img_process_batch_tab_callbacks = []
+img2img_process_batch_tab_each_callbacks = []
+img2img_postprocess_batch_tab_each_callbacks = []
+img2img_postprocess_batch_tab_callbacks = []
+
+
+def img2img_process_batch_hijack(*args, **kwargs):
+    for callback in img2img_process_batch_tab_callbacks:
+        callback()
+
+    res_was_none = False
+    def img2img_scripts_run_hijack(*args, **kwargs):
+        nonlocal res_was_none
+        if res_was_none:
+            for callback in img2img_postprocess_batch_tab_each_callbacks:
+                callback()
+
+        for callback in img2img_process_batch_tab_each_callbacks:
+            callback()
+
+        res = original_img2img_scripts_run(*args, **kwargs)
+        if res is not None:
+            for callback in img2img_postprocess_batch_tab_each_callbacks:
+                callback()
+        else:
+            res_was_none = True
+
+        return res
+
+    original_img2img_scripts_run = scripts.scripts_img2img.run
+    scripts.scripts_img2img.run = img2img_scripts_run_hijack
+
+    try:
+        return getattr(img2img, '__batch_loopback_original_process_batch')(*args, **kwargs)
+    finally:
+        if res_was_none:
+            for callback in img2img_postprocess_batch_tab_each_callbacks:
+                callback()
+
+        scripts.scripts_img2img.run = original_img2img_scripts_run
+        for callback in img2img_postprocess_batch_tab_callbacks:
+            callback()
+
+
+if hasattr(img2img, '__batch_loopback_original_process_batch'):
+    # reset in case extension was updated
+    img2img.process_batch = getattr(img2img, '__batch_loopback_original_process_batch')
+
+setattr(img2img, '__batch_loopback_original_process_batch', img2img.process_batch)
+img2img.process_batch = img2img_process_batch_hijack
 
 
 class BatchLoopbackScript(scripts.Script):
+    def __init__(self):
+        self.img2img_batch_index = 0
+        self.is_img2img_batch = False
+        self.last_images = []
+        self.grow_last_images = True
+        self.current_image_index = 0
+
+        global img2img_process_batch_tab_callbacks, img2img_process_batch_tab_each_callbacks, img2img_postprocess_batch_tab_each_callbacks, img2img_postprocess_batch_tab_callbacks
+        img2img_process_batch_tab_callbacks.append(self.img2img_process_batch_tab)
+        img2img_process_batch_tab_each_callbacks.append(self.img2img_process_batch_tab_each)
+        img2img_postprocess_batch_tab_each_callbacks.append(self.img2img_postprocess_batch_tab_each)
+        img2img_postprocess_batch_tab_callbacks.append(self.img2img_postprocess_batch_tab)
+
     def title(self):
         return 'Batch Loopback'
 
@@ -20,36 +78,83 @@ class BatchLoopbackScript(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
-        global img2img_batch_loopback
-        local_img2img_batch_loopback = gr.Checkbox(visible=False)
-        img2img_batch_loopback.change(
-            fn=lambda x: x,
-            inputs=[img2img_batch_loopback],
-            outputs=[local_img2img_batch_loopback])
-        return [local_img2img_batch_loopback]
+        if not is_img2img:
+            return [gr.State(0.0)]
 
-    def postprocess_batch(self, p: processing.StableDiffusionProcessing, img2img_batch_loopback, **kwargs):
+        with gr.Accordion(label='Batch Loopback', open=False, elem_id='batch_loopback'):
+            loopback_mix = gr.Slider(
+                label='Loopback mix',
+                value=0.0,
+                minimum=0.0,
+                maximum=1.0,
+                step=0.01,
+                elem_id='batch_loopback_mix')
+
+        return [loopback_mix]
+
+    def process(self, p, loopback_mix):
         if not isinstance(p, processing.StableDiffusionProcessingImg2Img): return
-        if not img2img_batch_loopback: return
+        if not loopback_mix: return
+        if not self.is_img2img_batch:
+            self.last_images.clear()
+            self.current_image_index = 0
 
-        image = torchvision.transforms.ToPILImage()(kwargs['images'][0])
+    def process_batch(self, p, loopback_mix, **kwargs):
+        if not isinstance(p, processing.StableDiffusionProcessingImg2Img): return
+        if not loopback_mix: return
+        if self.is_img2img_batch:
+            if self.img2img_batch_index == 0: return
+        else:
+            if not self.last_images: return
+
+        last_batch = self.last_images[self.current_image_index if self.is_img2img_batch else self.current_image_index - 1]
+        current_latent = p.init_latent
+        current_images = list(p.init_images)
+
         p.init_images.clear()
-        p.init_images.append(image)
+        p.init_images.extend(last_batch)
         with devices.autocast():
             p.init(p.all_prompts, p.all_seeds, p.all_subseeds)
 
+        p.init_images.clear()
+        p.init_images.extend(current_images)
 
-def on_after_component(component, **_kwargs):
-    global img2img_batch_loopback
-    if getattr(component, 'elem_id', None) == 'img2img_tiling':
-        img2img_batch_loopback.render()
-        return
+        p.init_latent = p.init_latent * loopback_mix + current_latent * (1.0 - loopback_mix)
+        pass
 
-    # use existing checkbox if/when webui implements it
-    if getattr(component, 'img2img_batch_loopback', None) == 'img2img_batch_loopback':
-        img2img_batch_loopback.unrender()
-        img2img_batch_loopback = component
-        return
+    def postprocess_batch(self, p, loopback_mix, **kwargs):
+        if not isinstance(p, processing.StableDiffusionProcessingImg2Img): return
+        if not loopback_mix: return
 
+        if not self.is_img2img_batch:
+            if shared.state.job_no == shared.state.job_count - 1:
+                self.last_images.clear()
+                self.current_image_index = 0
+                return
 
-script_callbacks.on_after_component(on_after_component)
+        images = [torchvision.transforms.ToPILImage()(image) for image in kwargs['images']]
+        if self.grow_last_images:
+            self.last_images.append(images)
+        else:
+            self.last_images[self.current_image_index] = images
+
+        self.current_image_index += 1
+
+    def img2img_process_batch_tab(self):
+        self.img2img_batch_index = 0
+        self.is_img2img_batch = True
+        self.last_images.clear()
+        self.grow_last_images = True
+
+    def img2img_process_batch_tab_each(self):
+        self.current_image_index = 0
+
+    def img2img_postprocess_batch_tab_each(self):
+        self.img2img_batch_index += 1
+        self.grow_last_images = False
+
+    def img2img_postprocess_batch_tab(self):
+        self.img2img_batch_index = 0
+        self.is_img2img_batch = False
+        self.last_images.clear()
+        self.grow_last_images = True
